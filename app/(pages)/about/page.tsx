284622d68b94110f95f2e057593907d423f2d287
KINOTheProducer <kinotheproducer@gmail.com>
2023-12-15 21:40:09 -0500

    setup layout for about page, added about image to layout

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
+import AboutImage from '@/components/img/about.png';
+import Image from 'next/image';
 
 const About = () => {
   return (
     <div className='container-page'>
-      Lorem Ipsussy. Lorem Ipsussy. Lorem Ipsussy. Lorem
-      Ipsussy. Lorem Ipsussy. Lorem Ipsussy. Lorem Ipsussy.
-      Lorem Ipsussy. Lorem Ipsussy. Lorem Ipsussy. Lorem
-      Ipsussy.
+      <div className='flex flex-row gap-6 items-center'>
+        <Image
+          src={AboutImage}
+          width={300}
+          alt='About Avatar'
+          className='rounded-full'
+        />
+        <div className=''>
+          <h1 className='text-6xl py-3 font-bold text-left flex'>
+            about me
+          </h1>
+          <p className='text-left'>
+            Lorem Ipsussy. Lorem Ipsussy. Lorem Ipsussy.
+            Lorem Ipsussy. Lorem Ipsussy. Lorem Ipsussy.
+            Lorem Ipsussy. Lorem Ipsussy. Lorem Ipsussy.
+            Lorem Ipsussy. Lorem Ipsussy.
+            <br /> <br />
+            Lorem Ipsussy. Lorem Ipsussy. Lorem Ipsussy.
+            Lorem Ipsussy. Lorem Ipsussy. Lorem Ipsussy.
+            Lorem Ipsussy. Lorem Ipsussy. Lorem Ipsussy.
+            Lorem Ipsussy. Lorem Ipsussy.
+            <br /> <br />
+            Lorem Ipsussy. Lorem Ipsussy. Lorem Ipsussy.
+            Lorem Ipsussy. Lorem Ipsussy. Lorem Ipsussy.
+            Lorem Ipsussy. Lorem Ipsussy. Lorem Ipsussy.
+            Lorem Ipsussy. Lorem Ipsussy.
+            <br /> <br />
+            Lorem Ipsussy. Lorem Ipsussy. Lorem Ipsussy.
+            Lorem Ipsussy. Lorem Ipsussy. Lorem Ipsussy.
+            Lorem Ipsussy. Lorem Ipsussy. Lorem Ipsussy.
+            Lorem Ipsussy. Lorem Ipsussy.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
