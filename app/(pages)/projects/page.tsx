a4240714a7d96c47208c1979cdaee9b43e59e978
KINOTheProducer <kinotheproducer@gmail.com>
2023-12-18 20:39:56 -0500

    added carousel to project cards, 3 on each page with left and right arrows for pagination, changed global styling on container-page

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import {
   Card,
@@ -13,23 +15,59 @@ import { Separator } from '@/components/ui/separator';
 import Image from 'next/image';
 import Link from 'next/link';
 import { CardContentData } from './CardContentData';
+import { useState } from 'react';
 
 const Projects = () => {
+  const projectsPerPageLG = 3;
+  const projectsPerPageMD = 2;
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const projectsPerPage =
+    window.innerWidth < 1300
+      ? projectsPerPageMD
+      : projectsPerPageLG;
+
+  const totalProjects = CardContentData.length;
+  const totalPages = Math.ceil(
+    totalProjects / projectsPerPage,
+  );
+
+  const handleNextPage = () => {
+    setCurrentPage(
+      (prevPage) => (prevPage + 1) % totalPages,
+    );
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage(
+      (prevPage) =>
+        (prevPage - 1 + totalPages) % totalPages,
+    );
+  };
+
+  const startIndex = currentPage * projectsPerPage;
+  const endIndex = startIndex + projectsPerPage;
+
+  const visibleProjects = CardContentData.slice(
+    startIndex,
+    endIndex,
+  );
+
   return (
     <div className='container-page'>
-      <div className='flex flex-col gap-6'>
-        <div>
-          <h1 className='text-6xl font-bold mb-5 text-left'>
-            Projects
+      <div className='flex flex-col flex-wrap gap-6 w-full'>
+        <div className='w-full'>
+          <h1 className='text-5xl md:text-6xl font-bold my-10 text-left'>
+            projects
           </h1>
-          <p className='text-left text-xl mb-3'>
-            This is a collection of projects I have worked
-            on as a developer. They range from frontend to
+          <p className='text-left md:text-xl mb-3'>
+            this is a collection of projects I have worked
+            on as a developer. they range from frontend to
             backend development, as well as a mix of both,
             with fullstack development.
           </p>
           Filter by:
-          <div className='flex flex-row gap-3 align-center justify-center items-center my-1'>
+          <div className='flex flex-row gap-3 w-full align-center justify-center items-center my-1'>
             <Button
               color='primary'
               className='font-semibold'
@@ -51,12 +89,15 @@ const Projects = () => {
           </div>
         </div>
         <div>
-          <div className='flex flex-row gap-6'>
-            {CardContentData.map((data) => (
+          <div className='flex flex-row flex-wrap place-content-center gap-3 w-full'>
+            {/* {CardContentData.map((data) => ( */}
+            {visibleProjects.map((data) => (
               <Card key={data.id}>
                 <CardHeader>
-                  <CardTitle>{data.title}</CardTitle>
-                  <CardDescription className='text-left'>
+                  <CardTitle className='text-xl'>
+                    {data.title}
+                  </CardTitle>
+                  <CardDescription>
                     {data.description}
                   </CardDescription>
                 </CardHeader>
@@ -66,15 +107,14 @@ const Projects = () => {
                       src={data.image}
                       width={300}
                       height={300}
-                      //   change this later
-                      alt='testing'
+                      alt={data.title}
                     />
                   </div>
-                  <div className='flex flex-row gap-1'>
+                  <div className='flex flex-row gap-1 place-content-center'>
                     {data.stack.map((stack) => (
                       <Badge
                         key={stack}
-                        color='outline'
+                        variant='secondary'
                       >
                         {stack}
                       </Badge>
@@ -82,15 +122,35 @@ const Projects = () => {
                   </div>
                 </CardContent>
                 <CardFooter className='place-content-end'>
-                  <Button
-                    variant='secondary'
-                    className='font-semibold'
-                  >
-                    <Link href={data.link}>Live Demo</Link>
-                  </Button>
+                  <Link href={data.link}>
+                    <Button
+                      variant='secondary'
+                      className='font-semibold'
+                    >
+                      Live Demo
+                    </Button>
+                  </Link>
                 </CardFooter>
               </Card>
             ))}
+          </div>
+          <div className='flex justify-between mt-5 text-right'>
+            {currentPage > 0 && (
+              <Button
+                variant='secondary'
+                onClick={handlePrevPage}
+              >
+                {'<'}
+              </Button>
+            )}
+            {currentPage < totalPages - 1 && (
+              <Button
+                variant='secondary'
+                onClick={handleNextPage}
+              >
+                {'>'}
+              </Button>
+            )}
           </div>
         </div>
       </div>
